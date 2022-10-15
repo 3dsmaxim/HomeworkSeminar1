cfee97f9d4b21228edaf3e9bdad861c70f166bb6
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-10-15 11:37:25 +0300

    add Task_006 and completed

---
+++ Task_006/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.WriteLine("Введите число: ");
+int MrA = Convert.ToInt32(Console.ReadLine());
+int MrB = MrA % 2;
+if (MrB == 0)
+{
+    Console.WriteLine("Число: " + MrA + " четное.");
+}
+else
+{
+    Console.WriteLine("Число: " + MrA + " нечетное.");
+}
