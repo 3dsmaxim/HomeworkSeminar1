eb81c19a01dcf9a51d18e89cac1bc6b0c2b623c5
Maxim Stepanyuk <3dsmaxim@mail.ru>
2022-10-15 20:42:42 +0300

    Ичправил замечания in Task_008

--- Task_008/Program.cs
+++ Task_008/Program.cs
@@ -4,51 +4,80 @@
 // а на выходе показывает все чётные числа от 1 до N.
 
 
-Console.WriteLine("Введите число от 4 до *как получится* ");
-int MrA = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-int MrB = MrA;
+// Console.WriteLine("Введите число от 4 до *как получится* ");
+// int MrA = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
+// int MrB = MrA;
 
-if (MrA >= 4)
+// if (MrA >= 4)
+// {
+//     while (count <= (MrA / 2) + (MrA % 2) - 2)
+//     {
+//         if (MrA % 2 == 0)
+//         {
+
+//             Console.Write(MrB - 2 + ", ");
+//             MrB = MrB - 2;
+//             count++;
+//         }
+//         else
+//         {
+//             MrB = MrB + 1;
+//             while (count <= (MrA / 2) + (MrA % 2) - 2)
+//             {
+
+//                 if (MrA % 2 == 1)
+//                 {
+
+//                     Console.Write(MrB - 2 + ", ");
+//                     MrB = MrB - 2;
+//                     count++;
+
+
+//                 }
+
+//             }
+
+//         }
+
+//     }
+// }
+
+//     else
+// {
+//     Console.WriteLine("Ну что ты что ты пишешь... от 4 ВКЛЮЧИТЕЛЬНО!!!");
+
+
+
+// }
+
+
+Console.WriteLine("Введите число от 3 до *как получится* ");
+int mrA = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (mrA >= 3)
 {
-    while (count <= (MrA / 2) + (MrA % 2) - 2)
+    while (count < mrA - 2)
     {
-        if (MrA % 2 == 0)
+        if (mrA % 2 == 0)
         {
 
-            Console.Write(MrB - 2 + ", ");
-            MrB = MrB - 2;
-            count++;
+            Console.Write(count + 2 + ", ");
+            count = count + 2;
         }
         else
         {
-            MrB = MrB + 1;
-            while (count <= (MrA / 2) + (MrA % 2) - 2)
+            mrA = mrA + 1;
+            while (count < mrA - 2)
             {
-
-                if (MrA % 2 == 1)
-                {
-
-                    Console.Write(MrB - 2 + ", ");
-                    MrB = MrB - 2;
-                    count++;
-
-
-                }
-
+                Console.Write(count + 2 + ", ");
+                count = count + 2;
             }
 
         }
-
     }
 }
-
-    else
+else
 {
-    Console.WriteLine("Ну что ты что ты пишешь... от 4 ВКЛЮЧИТЕЛЬНО!!!");
-
-
-
+    Console.WriteLine("Ну что ты что ты пишешь... от 3 ВКЛЮЧИТЕЛЬНО!!!");
 }
-
-
